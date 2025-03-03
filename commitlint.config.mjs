80df18be3e8c4845e0fc5f8bfec853da68ed60a0
Helio Frota <00hf11@gmail.com>
2025-03-03 11:24:16 -0300

    tests

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,7 +2,7 @@ export default {
   extends: ['@commitlint/config-conventional'],
   plugins: ['commitlint-plugin-function-rules'],
   rules: {
-    'type-empty':[0, 'always'],
-    'subject-min-length':[0, 'always']
+    'type-empty':[1, 'always'],
+    'subject-empty':[1, 'always']
   },
 }
